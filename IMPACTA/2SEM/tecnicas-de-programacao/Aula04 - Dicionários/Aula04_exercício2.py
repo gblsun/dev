51f3edaeff36acadffe34e5eecbb777c498bfbf7
Gabriel Pavanelli <gabriel.mp13@hotmail.com>
2024-08-20 09:42:50 -0300

    atualização aula 04 e exercícios aula04

---
+++ IMPACTA/2SEM/tecnicas-de-programacao/Aula04 - Dicionários/Aula04_exercício2.py
@@ -0,0 +1,15 @@
+# # Aula: técnicas de programação IMPACTA 16 e 17 de Agosta de 2024
+# Anotações e comentários feitos por Gabriel Muchon Pavanelli (github: gblsunn)
+
+# Novas estruturas de Dados:
+# Dicionários
+
+# Exercício II
+# Escreva uma função que conta a quantidade devogais em um texto e armazena tal quantidade emum dicionário, onde a chave é a vogal considerada e o valor é a quantidade de vezes que essa vogalaparece no texto. A função deve receber o texto como entrada e retornar o dicionário. Por exemplo, para o texto abaixo: texto = 'faculdade impacta de tecnologia' A função deve devolver o seguinte dicionário:{'a': 5, 'u': 1, 'e': 3, 'o' : 2, 'i': 2 }
+
+# %% ---begin
+texto = input('Digite seu texto: ')
+
+for i in texto.lower.isalpha():
+    print(texto)
+# %% ---end
