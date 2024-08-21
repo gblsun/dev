4c4c252c5a2d4b08c76490db8b94d9fe36c37b5e
Gabriel Pavanelli <gabriel.mp13@hotmail.com>
2024-08-20 21:09:57 -0300

    atualização aula04_exercicío2.py

--- IMPACTA/2SEM/tecnicas-de-programacao/Aula04 - Dicionários/Aula04_exercício2.py
+++ IMPACTA/2SEM/tecnicas-de-programacao/Aula04 - Dicionários/Aula04_exercício2.py
@@ -7,11 +7,10 @@
 # Exercício II
 # Escreva uma função que conta a quantidade devogais em um texto e armazena tal quantidade emum dicionário, onde a chave é a vogal considerada e o valor é a quantidade de vezes que essa vogalaparece no texto. A função deve receber o texto como entrada e retornar o dicionário. Por exemplo, para o texto abaixo: texto = 'faculdade impacta de tecnologia' A função deve devolver o seguinte dicionário:{'a': 5, 'u': 1, 'e': 3, 'o' : 2, 'i': 2 }
 
+# ainda não terminado
 # %% ---begin
-    
-texto = input('Digite seu texto: ')
-c for c in texto if (a, e, i, o, u):
-    print(texto)
-    
-    
+texto = input("Digite seu texto: ")
+vogais = {"a", "e", "i", "o", "u"}
+contagem = sum(1 for char in texto.lower() if char in 'a' if char in)
+print(contagem)
 # %% ---end
