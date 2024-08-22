843c76feedbba100ca93532f7be811ac4bafabd7
Gabriel Muchon Pavanelli <gabriel.mp13@hotmail.com>
2024-08-22 13:58:42 -0300

    Update exercicio2.py

--- IMPACTA/2SEM/tecnicas-de-programacao/Aula05/exercício2.py
+++ IMPACTA/2SEM/tecnicas-de-programacao/Aula05/exercício2.py
@@ -1,23 +1,26 @@
-# %%
-# imports
-
+# %% --begin
 import random
 
-# Funções
 
 def gerar_cpf():
     return "".join([str(random.randint(0, 9)) for _ in range(9)])
 
+
 def gerar_subconjunto(conjunto):
     return set(random.sample(list(conjunto), int(random.random() * 100)))
 
-conjunto_pessoas = set ()
-for i in range(100):
-    gerar_cpf()
-    conjunto_pessoas.add(gerar_cpf())
-    print(conjunto_pessoas)
 
+conjunto_pessoas = set()
 
-# %%
-# Definir conjunto vazio
-#
+while len(conjunto_pessoas) < 100:
+    cpf = gerar_cpf()
+    conjunto_pessoas.add(cpf)
+
+esportes = {
+    "surfe": gerar_subconjunto(conjunto_pessoas),
+    "volei": gerar_subconjunto(conjunto_pessoas),
+    "ginastica": gerar_subconjunto(conjunto_pessoas),
+    "judo": gerar_subconjunto(conjunto_pessoas),
+}
+
+# %% --end
