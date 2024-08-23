544451ef906b0bd5f95a91c18da4cc107a013f8c
Gabriel Muchon Pavanelli <gabriel.mp13@hotmail.com>
2024-08-23 13:30:22 -0300

    update aula1 e exercicio2

---
+++ IMPACTA/2SEM/tecnicas-de-programacao/Aula01 - Revisão/Aula01.py
@@ -0,0 +1,10 @@
+# Aula 01: técnicas de programação IMPACTA 8 e 9 de Agosta de 2024
+# Anotações e comentários feitos por Gabriel Muchon Pavanelli (github: gblsunn)
+
+# Revisão: Estruturas de controle de flux
+
+# Processo: os comandos nas linhas de código realizam uma ação específica.
+# Decisão: if, else e elif são as declarações mais comuns para decisão
+# Entrada manual: pode-se usar a função input() para obter entrada do usuário a partir do teclado.
+# Processo pré-definido/módulo: pode-se definir funções ou importar módulos para reutilizar código. Utiliza-se a palavra-chave def para criar suas próprias funções e import para importar módulos externos. Há também as funções embutidas do Python, que não precisam ser importadas.
+# Exibição na tela: pode-se usar a função print() para exibir informações na tela.
