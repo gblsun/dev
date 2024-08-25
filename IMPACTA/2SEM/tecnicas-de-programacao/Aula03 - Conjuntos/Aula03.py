9d5bf59814a8ad495c3ee70d26131cca80964336
Gabriel Muchon Pavanelli <gabriel.mp13@hotmail.com>
2024-08-25 11:44:51 -0300

    add Aula03-exercicio01.py

--- IMPACTA/2SEM/tecnicas-de-programacao/Aula03 - Conjuntos/Aula03.py
+++ IMPACTA/2SEM/tecnicas-de-programacao/Aula03 - Conjuntos/Aula03.py
@@ -1,6 +1,7 @@
 # Aula 03: técnicas de programação IMPACTA 15 de Agosto de 2024
 # Anotações e comentários feitos por Gabriel Muchon Pavanelli (github: gblsunn)
 
+# Novas estruturas de dados:  Conjuntos
 '''
 Sintaxe básica:
 
