b000ee7658bc93ee425def81bffa2082ca3c2351
gabriel.mp13@hotmail.com <gabriel.mp13@hotmail.com>
2024-09-06 13:30:15 -0300

    Update aula10

--- IMPACTA/2SEM/tecnicas-de-programacao/Aula10 - Bibliotecas/Aula10.py
+++ IMPACTA/2SEM/tecnicas-de-programacao/Aula10 - Bibliotecas/Aula10.py
@@ -78,12 +78,13 @@ from functools import lru_cache
 
 
 def fibonacci(n):
-    if n == 1:
-        return (0)
-    elif n == 2:
-        return (1)
-    else:
-        return(fibonacci(n-1)+ fibonacci(n-2))
+    for i in range(5):
+        if n == 1:
+            return (0)
+        elif n == 2:
+            return (1)
+        else:
+            return(fibonacci(n-1)+ fibonacci(n-2))
 
 
 @lru_cache
