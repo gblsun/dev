878638d726112aec5f602a60fba4a3f05c6b47c3
gabriel.mp13@hotmail.com <gabriel.mp13@hotmail.com>
2024-09-12 08:38:13 -0300

    add aula11
    aula bibliotecas e modulos

---
+++ IMPACTA/2SEM/tecnicas-de-programacao/Aula11/Aula11.py
@@ -0,0 +1,51 @@
+'''
+Aula 11: técnicas de programação IMPACTA 12 de Setembro de 2024
+Anotações e comentários feitos por Gabriel Muchon Pavanelli (github: gblsunn)
+
+Bibliotecas em Python (continuação)
+
+→
+↳
+
+Scripts
+
+→ Em Linguagem de Programação, começamos utilizando um interpretador interativo para testar os primeiros comandos da linguagem
+
+→ Posteriormente, passamos a utilizar o editor de texto para desenvolver e salvar nossos programas em arquivos.
+
+→ A estes arquivos, damos o nome de ->scripts<- .
+
+Módulos
+
+→ Conforme os projetos se tornam mais complexos, começa a ficar inviável utilizar um único arquivo para salvar todo o programa
+
+→ Dividimos o script em vários arquivos para facilitar a organização e manutenção do código.
+
+→ A estes arquivos, damos o nome de módulos
+
+→ Não há diferença “prática” entre script e módulo
+
+Pacotes
+
+→ Conforme os projetos se tornam ainda mais complexos, podemos precisar de muitos arquivos (módulos);
+
+→ Dividimos os módulos em pastas para, novamente, facilitar a organização e manutenção do código;
+
+→ À estas pastas, damos o nome de pacotes.
+
+Bibliotecas em python
+
+→ Em Python, bibliotecas são coleções de módulos que contêm funcionalidades reutilizáveis;
+
+→ Nas bibliotecas, os módulos geralmente são organizados em pacotes;
+
+→ Há três tipos de bibliotecas:
+
+    ↳ Integradas: math, datetime, ... ;
+
+    ↳ De terceiros: geralmente instaladas via pip;
+
+    ↳ Próprias;
+'''
+
+# %%
