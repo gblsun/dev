84b1bdfa8d26c0ceb964a7311ae0043d7bedc45f
Gabriel Muchon Pavanelli <gabriel.mp13@hotmail.com>
2024-09-27 09:38:54 -0300

    +update aula15

--- IMPACTA/2SEM/tecnicas-de-programacao/Aula15 - Análise de Eficiências de Algoritmos/Exercício.py
+++ IMPACTA/2SEM/tecnicas-de-programacao/Aula15 - Análise de Eficiências de Algoritmos/Exercício.py
@@ -10,17 +10,23 @@
 → Qual é o custo computacional (de acordo com a notação Big O) de uma função que implementa o algoritmo de busca binária?
 → A resposta anterior depende dos valores passados para os parâmetros da função?
 '''
+# complexidade de algoritmo logarítmica O (log n)
+# logaritmo de base 2 .
 
 # %% ---> begin
 # imports
+import random
+import numpy as np
+import matplotlib.pyplot as plt
 
 # funções
 def busca_binaria(lista, valor):
     inicio = 0
     fim = len(lista) - 1
-    passos=0
+    passos=0 # variavel contadora
+
     while inicio <= fim:
-        passos+=1
+        passos+=1 # variavel contadora cada instruções será executada
         meio = (inicio + fim) // 2 
 
         if lista[meio] == valor:
@@ -34,11 +40,16 @@ def busca_binaria(lista, valor):
 
     return -1, passos
 
+def criar_lista_aleatoria(tamanho, inicio, fim):
+    lista = [random.randint(inicio, fim) for _ in range(tamanho)]
+    return lista
+
 
 # programa principal
 # TODO: criar diferentes sequências, todas ordenadas, com tamanhos distintos
 # TODO: busca binaria por um valor que não está presente na sequência
 # Traçar um gráfico len(lista)x quantidade de passos
-    
+
+
 
 # %% ---> end
