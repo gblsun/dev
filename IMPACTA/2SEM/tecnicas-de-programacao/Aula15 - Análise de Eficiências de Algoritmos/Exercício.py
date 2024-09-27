285234ade59673a5eae889fe16670ba7861f4703
Gabriel Muchon Pavanelli <gabriel.mp13@hotmail.com>
2024-09-27 09:02:01 -0300

    Add aula15b.py e exercícios

---
+++ IMPACTA/2SEM/tecnicas-de-programacao/Aula15 - Análise de Eficiências de Algoritmos/Exercício.py
@@ -0,0 +1,15 @@
+'''
+<--x-->+<--x-->+<--x-->+<--x-->+<--x-->+<--x-->+<--x-->+<--x-->+<--x-->+<--x-->+<--x-->+
+|                                                                                      |
+| Aula 15: técnicas de programação IMPACTA 27 de Setembro de 2024                      |
+| Anotações e comentários feitos por Gabriel Muchon Pavanelli (github: gblsunn)        |
+|                                                                                      |
+<--x-->+<--x-->+<--x-->+<--x-->+<--x-->+<--x-->+<--x-->+<--x-->+<--x-->+<--x-->+<--x-->+
+→ 
+↳
+Qual é o custo computacional (de acordo com a
+notação Big O) de uma função que implementa o
+algoritmo de busca binária?
+● A resposta anterior depende dos valores passados
+para os parâmetros da função?
+'''
