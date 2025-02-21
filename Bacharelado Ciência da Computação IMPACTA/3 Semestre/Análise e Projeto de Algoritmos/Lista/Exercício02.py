8fe626977101d9a722e9d2ea510e28089935a424
gblsunn <gabriel.mp13@hotmail.com>
2025-02-21 12:15:47 -0300

    +update

---
+++ Bacharelado Ciência da Computação IMPACTA/3 Semestre/Análise e Projeto de Algoritmos/Lista/Exercício02.py
@@ -0,0 +1,11 @@
+"""
+Exercício 2 - Desenvolvido por Gabriel Muchon Pavanelli. Faculdade Impacta 21/02/2025.
+
+2 - Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boasvindas para ela:
+Ex:
+Qual é o seu nome? João da Silva
+Olá João da Silva, é um prazer te conhecer!
+"""
+
+nome = input("Qual é o seu nome?")
+print("Ola", nome, ", é um prazer te conhecer!")
