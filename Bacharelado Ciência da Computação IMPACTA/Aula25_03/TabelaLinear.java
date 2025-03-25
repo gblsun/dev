5b44eae32d2858e3a1d1a18032f9965a72592fae
gblsunn <gabriel.mp13@hotmail.com>
2025-03-25 08:38:31 -0300

    Adicionado TabelaLinear.java

---
+++ Bacharelado Ciência da Computação IMPACTA/Aula25_03/TabelaLinear.java
@@ -0,0 +1,19 @@
+/*
+Aula 25/03/2025 - Anotações por Gabriel Muchon Pavanelli.
+Faculdade Impacta 25/05/2025.
+
+ */
+package Aula25_03;
+
+public class TabelaLinear {
+    public static void main(String[] args) {
+
+        // Definindo tabela Linear
+
+        int[] array = {90, 70, 50, 80, 60, 85};
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i] + ",");
+        }
+    }
+}
